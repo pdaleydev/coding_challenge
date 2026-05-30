9c8a77ecf59b46febe7b8cbb3bce87aca5059b39
pattyd-dev <pattydserver@gmail.com>
2026-05-30 11:04:57 -0400

    New application insights logic. Adding workspace ID for CICD, needed for logging.

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,14 +36,33 @@ resource "azurerm_storage_account" "sa" {
 # ---------------------------------------------------------------------------
 # Application Insights
 # ---------------------------------------------------------------------------
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "log-${var.project}-${var.environment}-${var.location_short}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = var.tags
+}
+
 resource "azurerm_application_insights" "ai" {
   name                = "appi-${var.project}-${var.environment}-${var.location_short}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.law.id
 
   tags = var.tags
 }
+# resource "azurerm_application_insights" "ai" {
+#   name                = "appi-${var.project}-${var.environment}-${var.location_short}"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   application_type    = "web"
+# 
+#   tags = var.tags
+# }
 
 # ---------------------------------------------------------------------------
 # Consumption Plan (Free Tier — Y1)
