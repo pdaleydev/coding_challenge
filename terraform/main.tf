68a7a839d7a4c2b2182d41259d486593a272dcf9
pattyd-dev <pattydserver@gmail.com>
2026-05-30 12:26:59 -0400

    Testing change to different consumption plan to add private endpoint.

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,7 +72,8 @@ resource "azurerm_service_plan" "plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Windows"
-  sku_name            = "Y1" # Consumption (Free) plan
+  # sku_name            = "Y1" # Consumption (Free) plan
+  sku_name = "EP1" # Testing for adding Private Endpoint.
 
   tags = var.tags
 }
