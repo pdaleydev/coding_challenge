09da0971f155cbf2ce73067694ac10add13b8e04
pattyd-dev <pattydserver@gmail.com>
2026-05-29 21:52:32 -0400

    Switched to remote state stored in Blob Storage.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -8,10 +8,10 @@ terraform {
       }
     }
 
-#   backend "azurerm" {
-#     resource_group_name  = "azure-terraform-states"           # resource group holding the storage account
-#     storage_account_name = "pdaleyterraformstates"         # the storage account you manually created
-#     container_name       = "tfstate"              # the blob container inside that account
-#     key                  = "hlwrld-dev.tfstate"   # filename for this project's state file
-#   }
+    backend "azurerm" {
+      resource_group_name  = "azure-terraform-states"           # resource group holding the storage account
+      storage_account_name = "pdaleyterraformstates"         # the storage account you manually created
+      container_name       = "tfstate"              # the blob container inside that account
+      key                  = "hlwrld-dev.tfstate"   # filename for this project's state file
+    }
 }
